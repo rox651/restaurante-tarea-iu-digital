f47468138f11e75fc2c13e8fd8b63cbc30134ef0
homero <homerojose69@gmail.com>
2025-05-21 14:02:40 -0500

    fix: update handler error image

--- src/views/restaurants/Restaurant.tsx
+++ src/views/restaurants/Restaurant.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import type { Restaurant as RestaurantType } from "../../domain/restaurant";
 
 const defaultImage = new URL(
@@ -12,15 +11,15 @@ type Props = {
 
 const Restaurant = ({ restaurant }: Props) => {
   const { name, address, imageURL, description } = restaurant;
-  const [imageToShow, setImageToShow] = useState(imageURL);
 
   return (
     <article className="bg-white shadow-md rounded-lg">
       <img
-        src={imageToShow}
+        src={imageURL}
         alt={name}
-        onError={() => {
-          setImageToShow(defaultImage);
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null; // prevents looping
+          currentTarget.src = defaultImage; // fallback image
         }}
         className="w-full h-48 object-cover rounded-t-lg"
       />
